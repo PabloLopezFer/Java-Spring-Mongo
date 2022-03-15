91d0b753b1ba7c403a7f733186ccb3722488287b
pablo_lopez <pablo_lopez@GTN2050>
2022-03-15 10:16:12 +0100

    Tests

--- src/test/java/com/example/restservice/webTestClient/WebClientDemoApplicationTests.java
+++ src/test/java/com/example/restservice/webTestClient/WebClientDemoApplicationTests.java
@@ -1,14 +1,21 @@
 package com.example.restservice.webTestClient;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 
 import com.example.restservice.dto.Book;
 
@@ -21,10 +28,29 @@ public class WebClientDemoApplicationTests {
 	@Autowired
 	private WebTestClient webTestClient;
 	
+	@Value("${spring.data.mongodb.uri}")
+    private static String uri;
+
+    @Value("${spring.data.mongodb.database}")
+    private static String database;
+    
+    @Container
+  	public static MongoDBContainer container = new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+
+    @DynamicPropertySource
+    public static void mongoDbProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", container::getReplicaSetUrl);
+    }
+    
+    @BeforeAll
+    public static void init() {
+        container.start();
+    }
+	
 	@Test
 	@Order(1)
 	public void test01_post() {
-		Book book = new Book("happyP", 20, "Harry Potter");
+		Book book = new Book("harryP", 20, "Harry Potter");
 		
 		webTestClient.post().uri("/books")
 				.contentType(MediaType.APPLICATION_JSON)
@@ -35,7 +61,7 @@ public class WebClientDemoApplicationTests {
 				.expectHeader().contentType(MediaType.APPLICATION_JSON)
 				.expectBody()
 				.jsonPath("$.id").isNotEmpty()
-				.jsonPath("$.id").isEqualTo("happyP");
+				.jsonPath("$.id").isEqualTo("harryP");
 	}
 	
 	@Test
@@ -53,7 +79,7 @@ public class WebClientDemoApplicationTests {
 	@Order(3)
 	public void test03_getByKey() {
 		webTestClient.get()
-				.uri("/books/{id}", "happyP")
+				.uri("/books/{id}", "harryP")
 				.exchange()
 				.expectStatus().isOk()
 				.expectBody()
@@ -64,9 +90,9 @@ public class WebClientDemoApplicationTests {
 	@Test
 	@Order(4)
 	public void test04_put() {
-		Book newBook = new Book("happyP", 50, "Harry potter 4");
+		Book newBook = new Book("harryP", 50, "Harry potter 4");
 		webTestClient.put()
-				.uri("/books/{id}", "happyP")
+				.uri("/books/{id}", "harryP")
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON)
 				.body(Mono.just(newBook), Book.class)
@@ -74,7 +100,7 @@ public class WebClientDemoApplicationTests {
 				.expectStatus().isOk()
 				.expectHeader().contentType(MediaType.APPLICATION_JSON)
 				.expectBody()
-				.jsonPath("$.id").isEqualTo("happyP");
+				.jsonPath("$.id").isEqualTo("harryP");
 	}
 	
 	
@@ -82,7 +108,7 @@ public class WebClientDemoApplicationTests {
 	@Order(5)
 	public void test05_delete() {
 		webTestClient.delete()
-				.uri("/books/{id}", "happyP")
+				.uri("/books/{id}", "harryP")
 				.exchange()
 				.expectStatus().isNoContent();
 	}
